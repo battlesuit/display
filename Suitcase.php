f78b9d508f7ee74e44286da98a72a9f2eb9f9211
Thomas Monzel <tom.monzel@gmail.com>
2012-11-20 21:34:19 +0100

    Moved autoloading to new Suitcase.php

---
+++ Suitcase.php
@@ -0,0 +1,6 @@
+<?php
+namespace suitcase {
+  autoload('display\Template', __DIR__."/lib/display/template.php");
+  autoload('display\html\Helpers', __DIR__."/lib/display/html/helpers.php");
+}
+?>
