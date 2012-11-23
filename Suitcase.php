c4e91f2b490fe75ba12171eeca7b7ea7520420c3
Thomas Monzel <tom.monzel@gmail.com>
2012-11-23 01:10:37 +0100

    Created init directory

--- Suitcase.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace suitcase {
-  autoload('display\Template', __DIR__."/lib/display/template.php");
-  autoload('display\html\Helpers', __DIR__."/lib/display/html/helpers.php");
-}
-?>
---
+++ init/module.php
@@ -0,0 +1,6 @@
+<?php
+namespace bs {
+  autoload('display\Template', dirname(__DIR__)."/lib/display/template.php");
+  autoload('display\html\Helpers', dirname(__DIR__)."/lib/display/html/helpers.php");
+}
+?>
